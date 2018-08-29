d0e72fdf576f69f9baf441094c43825e4c9d511d
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-08-30 09:57:43 +1000

    Moved api calls to a different file

--- adapters/blockchain/clients/btc/client.go
+++ adapters/blockchain/clients/btc/client.go
@@ -3,21 +3,16 @@ package btc
 import (
 	"bytes"
 	"encoding/hex"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net"
-	"net/http"
-	"net/url"
-	"strings"
 	"time"
+
+	"github.com/btcsuite/btcutil"
 
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/txscript"
 	"github.com/btcsuite/btcd/wire"
-	"github.com/btcsuite/btcutil"
 	"github.com/republicprotocol/renex-swapper-go/adapters/configs/keystore"
 	"github.com/republicprotocol/renex-swapper-go/adapters/configs/network"
 )
@@ -58,36 +53,11 @@ func (conn *Conn) PromptPublishTx(tx *wire.MsgTx, name string) (*chainhash.Hash,
 		return nil, err
 	}
 	stx := hex.EncodeToString(buf.Bytes())
-	data := url.Values{}
-	data.Set("tx", stx)
-
-	client := &http.Client{}
-	r, err := http.NewRequest("POST", conn.URL+"/pushtx", strings.NewReader(data.Encode())) // URL-encoded payload
-	if err != nil {
+	if err := conn.SubmitSignedTransaction(stx); err != nil {
 		return nil, err
 	}
-	r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
-	_, err = client.Do(r)
-	if err != nil {
-		return nil, err
-	}
-
 	hash := tx.TxHash()
 	return &hash, nil
-}
-
-func normalizeAddress(addr string, defaultPort string) (hostport string, err error) {
-	host, port, origErr := net.SplitHostPort(addr)
-	if origErr == nil {
-		return net.JoinHostPort(host, port), nil
-	}
-	addr = net.JoinHostPort(addr, defaultPort)
-	_, _, err = net.SplitHostPort(addr)
-	if err != nil {
-		return "", origErr
-	}
-	return addr, nil
 }
 
 func walletPort(params *chaincfg.Params) string {
@@ -106,8 +76,10 @@ func walletPort(params *chaincfg.Params) string {
 func (conn *Conn) SignTransaction(tx *wire.MsgTx, key keystore.Key) (*wire.MsgTx, bool, error) {
 	addr, err := key.GetAddress()
 	if err != nil {
-		return nil, false, fmt.Errorf("failed to decode your address: %v", err)
+		return nil, false, fmt.Errorf("your keystore is "+
+			"malformed %v", conn.ChainParams.Name)
 	}
+
 	myAddr, err := btcutil.DecodeAddress(string(addr), conn.ChainParams)
 	if err != nil {
 		return nil, false, fmt.Errorf("your address is not "+
@@ -120,21 +92,22 @@ func (conn *Conn) SignTransaction(tx *wire.MsgTx, key keystore.Key) (*wire.MsgTx
 		value = value + j.Value
 	}
 
-	unspentValue, err := conn.Balance(myAddr)
+	unspentValue, err := conn.Balance(string(addr))
 	if err != nil {
 		return nil, false, err
 	}
 
-	utxos, err := conn.ListUnspent(myAddr)
+	utxos, err := conn.GetUnspentOutputs(string(addr))
 	if err != nil {
 		return nil, false, err
 	}
 
 	if value > unspentValue {
-		return nil, false, fmt.Errorf("Not enough balance required:%d current:%d", value, unspentValue)
+		return nil, false, fmt.Errorf("Not enough balance"+
+			"required:%d current:%d", value, unspentValue)
 	}
 
-	for _, j := range utxos.UnspentOutputs {
+	for _, j := range utxos.Outputs {
 		if value <= 0 {
 			break
 		}
@@ -179,79 +152,31 @@ func (conn *Conn) SignTransaction(tx *wire.MsgTx, key keystore.Key) (*wire.MsgTx
 	return tx, true, nil
 }
 
-type Unspent struct {
-	TxID         string `json:"tx_hash"`
-	Vout         uint32 `json:"tx_output_n"`
-	ScriptPubKey string `json:"script"`
-	Amount       int64  `json:"value"`
-}
-
-type Unspents struct {
-	UnspentOutputs []Unspent `json:"unspent_outputs"`
-}
-
-func (conn *Conn) ListUnspent(address btcutil.Address) (Unspents, error) {
-	resp, err := http.Get(fmt.Sprintf(conn.URL + "/unspent?active=" + address.EncodeAddress()))
-	if err != nil {
-		return Unspents{}, err
-	}
-	defer resp.Body.Close()
-	utxoBytes, err := ioutil.ReadAll(resp.Body)
-	utxos := Unspents{}
-	json.Unmarshal(utxoBytes, &utxos)
-	return utxos, nil
-}
-
-func (conn *Conn) Balance(address btcutil.Address) (int64, error) {
-	utxos, err := conn.ListUnspent(address)
+func (conn *Conn) Balance(address string) (int64, error) {
+	utxos, err := conn.GetUnspentOutputs(address)
 	if err != nil {
 		return -1, err
 	}
 	var balance int64
-	for _, utxo := range utxos.UnspentOutputs {
+	for _, utxo := range utxos.Outputs {
 		balance = balance + utxo.Amount
 	}
 	return balance, nil
 }
 
-// WaitTillMined doesnot wait for the transactions to be mined, can be updated
-// later
+// WaitTillMined waits for the transactions to be mined, and gets the given
+// number of confirmations.
 func (conn *Conn) WaitTillMined(txHash *chainhash.Hash, confirmations int64) error {
-	type tx struct {
-		BlockHeight int64 `json:"block_height"`
-	}
-
-	type block struct {
-		Height int64 `json:"height"`
-	}
-
-	if confirmations <= 0 {
-		return nil
-	}
-	confirmations = confirmations - 1
 	for {
-		resp, err := http.Get(fmt.Sprintf(conn.URL + "/rawtx/" + txHash.String()))
+		mined, err := conn.Mined(txHash.String(), confirmations)
 		if err != nil {
 			return err
 		}
-		defer resp.Body.Close()
-		txBytes, err := ioutil.ReadAll(resp.Body)
-		transaction := tx{}
-		json.Unmarshal(txBytes, &transaction)
 
-		resp2, err := http.Get(fmt.Sprintf(conn.URL + "/latestblock"))
-		if err != nil {
-			return err
+		if mined {
+			return nil
 		}
-		defer resp2.Body.Close()
-		blockBytes, err := ioutil.ReadAll(resp2.Body)
-		blockDetails := block{}
-		json.Unmarshal(blockBytes, &blockDetails)
 
-		if transaction.BlockHeight != 0 && blockDetails.Height-transaction.BlockHeight >= confirmations {
-			break
-		}
 		time.Sleep(1 * time.Second)
 	}
-	return nil
 }
