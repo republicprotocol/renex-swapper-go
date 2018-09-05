b42aca796af6d124302c8ffb1bcb13ffbe4d70ef
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-09-05 19:17:21 +1000

    Updated swapper script

--- cmd/installer/macos/swapper.sh
+++ cmd/installer/macos/swapper.sh
@@ -1,9 +1,9 @@
-#!bin/bash
+#!/bin/bash
 #Start RenEx Atomic Swapper only if it is not running
-if [ “$(ps -ef | grep -v grep | grep swapper | wc -l)” -le 0 ] 
+if [ "$(ps -ef | grep -v grep | grep swapper | wc -l)" -le 0 ] 
 then
  # Note starting swapper not as a sudoer
- ./bin/swapper
+ ~/.swapper/bin
  echo "RenEx Atomic Swapper Started"
 else
  echo "RenEx Atomic Swapper Already Running"
